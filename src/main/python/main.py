f7eae25b02ec5df1a27f3a8108392774a3a416bf
duncanwedig <duncan.wedig@gmail.com>
2018-06-27 09:28:07 -0700

    Did a bit of restructuring of python stuff

---
+++ src/main/python/main.py
@@ -0,0 +1,7 @@
+from robot.write_gains import write_gains
+
+# This is just to hopefully make the structure make a bit more sense, where write_gains is year-specific and is
+# actually in the robot folder, which is intended to be year specific
+# Also, this allows for other types of Python based testing and/or writing and/or generation to occur
+# Honestly, it probably makes a bit less sense, though
+write_gains()
---
+++ src/main/python/robot/write_gains.py
@@ -0,0 +1,18 @@
+
+def write_gains():
+    import numpy as np
+    from utilities.gains_writer import GainsWriter, numpy_to_jama_matrix
+    from utilities.state_space_gains import GainsList, StateSpaceGains, default_gains
+    from utilities.state_space_utils import observability, controllability
+    from robot import motor
+
+
+    # Working directory when the gradle task is run defaults to project root
+    # Multiple output directories can be used in addition to multiple sets of gains
+    OUT_DIR = '.\\src\\main\\java\\frc\\team687\\robot\\constants\\'
+
+    # Create a GainsWriter from a GainsList
+    # In this instance, the subsystem in question is given its own individual Python file from which gains are created
+    writer = GainsWriter(motor.create_gains())
+    # # Write the gains to the files indicated by their names, in the directory indicated
+    writer.write_all(OUT_DIR)
