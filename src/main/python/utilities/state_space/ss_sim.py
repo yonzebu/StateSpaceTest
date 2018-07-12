aeac1c3161a61ba764caf7828540dfd69c815601
duncanwedig <duncan.wedig@gmail.com>
2018-07-12 14:29:48 -0700

    Better plotting that actually deals with arbitrary systems

--- src/main/python/utilities/state_space/ss_sim.py
+++ src/main/python/utilities/state_space/ss_sim.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 class StateSpaceControlSim(object):
 
-    def __init__(self, gains: GainsList, x_hat_initial, u_initial, x_initial, u_max, u_min):
+    def __init__(self, gains, x_hat_initial, u_initial, x_initial, u_max, u_min):
         assert isinstance(gains, GainsList) or isinstance(gains, StateSpaceGains), \
             "Gains must be a list of gains or a state space gains object"
         if isinstance(gains, StateSpaceGains):
@@ -27,6 +27,10 @@ class StateSpaceControlSim(object):
         self.y = self.current_gains.C * x_initial
         self.x_hat = x_hat_initial
 
+        self.num_states = self.current_gains.A.shape[0]
+        self.num_inputs = self.current_gains.B.shape[1]
+        self.num_sensor_inputs = self.current_gains.C.shape[0]
+
     def set_gains_index(self, index: int):
         self.gains_index = 0
         self.current_gains = self.gains.get_gains(self.gains_index)
@@ -42,26 +46,44 @@ class StateSpaceControlSim(object):
 
         return self.plant.x, self.u, self.y, self.x_hat
 
-    def plot(self, duration, plot_setttings, reference_calculator=lambda time: np.zeros((1, 1))):
+    def plot(self, duration, plot_setttings,
+             reference_calculator=(lambda time: np.zeros((1, 1)))):
 
-        x_list = []
-        v_list = []
-        u_list = []
-        y_list = []
-        x_hat_list = []
-        v_hat_list = []
+        # x, then u, then y, then x_hat
+        x_list = [[]] * self.num_states
+        u_list = [[]] * self.num_inputs
+        y_list = [[]] * self.num_sensor_inputs
+        x_hat_list = [[]] * self.num_states
 
         for t in np.arange(start=0., stop=duration, step=self.current_gains.dt):
             x, u, y, x_hat = self.update(reference_calculator(t))
-            x_list += [x[0, 0]]
-            v_list += [x[1, 0]]
-            u_list += [u[0, 0]]
-            y_list += [y[0, 0]]
-            x_hat_list += [x_hat[0, 0]]
-            v_hat_list += [x_hat[1, 0]]
+            for state_num in range(self.num_states):
+                x_list[state_num] = x_list[state_num] + [x[state_num, 0]]
+            for input_num in range(self.num_inputs):
+                u_list[input_num] = u_list[input_num] + [u[input_num, 0]]
+            for output_num in range(self.num_sensor_inputs):
+                y_list[output_num] = y_list[output_num] + [y[output_num, 0]]
+            for est_state_num in range(self.num_states):
+                x_hat_list[est_state_num] = x_hat_list[est_state_num] + [x_hat[est_state_num, 0]]
 
-        generated_vals = x_list, v_list, u_list, y_list, x_hat_list, v_hat_list
-        # x, v, u, y, x_hat, v_hat = generated_vals
+        generated_vals = [[]] * (2*self.num_states + self.num_sensor_inputs + self.num_inputs)
+        current_idx = 0
+        for i in range(self.num_states):
+            generated_vals[i] = x_list[i]
+        current_idx += self.num_states
+
+        for i in range(self.num_inputs):
+            generated_vals[current_idx + i] = u_list[i]
+        current_idx += self.num_inputs
+
+        for i in range(self.num_sensor_inputs):
+            generated_vals[current_idx + i] = y_list[i]
+        current_idx += self.num_sensor_inputs
+
+        for i in range(self.num_states):
+            generated_vals[current_idx + i] = x_hat_list[i]
+        
+        # x, u, y, x_hat, all expanded hopefully = generated_vals
         for i, flag in enumerate(plot_setttings):
             if flag:
                 plt.plot(generated_vals[i])
